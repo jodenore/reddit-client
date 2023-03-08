ee351cec95c8f65fc792afdb1bc94aa3f042499f
Jesse Odenore <info@jesseodenore.com>
2023-03-08 11:55:50 +0000

    Home now shows popular posts from all of reddit.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import PostList from "../features/posts/PostList";
+import { TestContext } from "../TestContext";
 import "./Home.css";
 const Home = () => {
+  const { posts } = useContext(TestContext);
+
   return (
     <div className="home-container">
-      <PostList />
+      <PostList posts={posts} />
     </div>
   );
 };
