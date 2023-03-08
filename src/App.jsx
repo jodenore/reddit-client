38f0116cdc6480b37f2a7c2fb2743aa1d585c60c
Jesse Odenore <info@jesseodenore.com>
2023-03-08 11:54:32 +0000

    Routes changed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Navbar from "./Components/NavBar/Navbar";
-import FullPost from "./pages/FullPost";
+import SearchResults from "./pages/SearchResults";
 import Home from "./pages/Home";
 import "./App.css";
+import { useContext } from "react";
+import { TestContext } from "./TestContext";
 function App() {
+  const { query } = useContext(TestContext);
   return (
     <div className="main-container">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/comments/:id" element={<FullPost />} />
+        <Route path={`/search`} element={<SearchResults />} />
       </Routes>
     </div>
   );
