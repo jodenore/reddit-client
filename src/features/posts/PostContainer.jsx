2b722112e05b749ee28c3ac828b0f237d2f97d0a
Jesse Odenore <info@jesseodenore.com>
2023-03-08 12:00:43 +0000

    Vote logic removed from container and added to Votes.jsx

--- src/features/posts/PostContainer.jsx
+++ src/features/posts/PostContainer.jsx
@@ -2,16 +2,13 @@ import React, { useState, useEffect } from "react";
 import Post from "./Post";
 
 const PostContainer = ({ post }) => {
-  const [upvote, setUpvote] = useState(false);
-  const [downvote, setDownvote] = useState(false);
-  const [votes, setVotes] = useState(null);
   const [isPicture, setIsPicture] = useState(false);
-
   useEffect(() => {
     if (post.url_overridden_by_dest) {
       if (
         post.url_overridden_by_dest.includes("jpg") ||
-        post.url_overridden_by_dest.includes("png")
+        post.url_overridden_by_dest.includes("png") ||
+        post.url_overridden_by_dest.includes("jpeg")
       ) {
         setIsPicture((prev) => (prev = true));
       }
