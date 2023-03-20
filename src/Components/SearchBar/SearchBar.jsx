ccf891977a872775b1500565ab8dd382e2e6b611
Jesse Odenore <info@jesseodenore.com>
2023-03-20 00:29:46 +0000

    search state is now global instead from the context

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,22 +1,30 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import { BsSearch } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
 import { createSearchParams, useNavigate } from "react-router-dom";
-import { TestContext } from "../../TestContext";
+import {
+  searchRedditPosts,
+  selectQuery,
+  setQuery,
+} from "../../features/posts/postsSlice";
 import "./SearchBar.css";
-
 const SearchBar = () => {
-  const { fetchRedditPosts, query, setQuery } = useContext(TestContext);
+  // Selecting the query from the global state
+  const query = useSelector(selectQuery);
+  // dispatch(react redux) and navigate(react router) functions
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleKeyPress = (e) => {
+    // Preventing the form from reloading the page
     e.preventDefault();
     if (query) {
-      fetchRedditPosts(query);
+      dispatch(searchRedditPosts(query));
       navigate({
         pathname: "/search",
         search: `${createSearchParams({ q: query }).toString()}`,
       });
-      setQuery("");
+      dispatch(setQuery(""));
     }
   };
 
@@ -26,7 +34,8 @@ const SearchBar = () => {
         className="transition"
         placeholder="Search for Posts"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        // Using the setQuery action from the global state
+        onChange={(e) => dispatch(setQuery(e.target.value))}
       />
       <button type={"submit"} className="search-icon">
         <BsSearch />
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,15 +1,21 @@
-import React, { useContext, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import PostList from "../features/posts/PostList";
-import { TestContext } from "../TestContext";
+import {
+  searchRedditPosts,
+  selectAllSearchResults,
+} from "../features/posts/postsSlice";
 const SearchResults = () => {
+  //Reads the query in state and adds it to the end of the url
   const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
-  const { searchResults, fetchRedditPosts } = useContext(TestContext);
+  const dispatch = useDispatch();
+  const searchResults = useSelector(selectAllSearchResults);
 
   useEffect(() => {
     if (query) {
-      fetchRedditPosts(query);
+      dispatch(searchRedditPosts(query));
     }
   }, []);
   return <PostList posts={searchResults} />;
