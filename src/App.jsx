ba7501cd43b28f8a01a82eade3ed49d966d7269e
Jesse Odenore <info@jesseodenore.com>
2023-04-04 18:42:41 +0100

    Implement ScrollToTop component for the page to scroll up on pathname, posts change

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import "./App.css";
 import "animate.css";
 import NotFound from "./pages/NotFound";
 import FullPostContainer from "./features/fullpost/FullPost/FullPostContainer";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <div className="main-container">
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path={`/search`} element={<SearchResults />} />
