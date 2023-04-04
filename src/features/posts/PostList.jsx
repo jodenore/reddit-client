036cb3aa9229e835d75db1295b1637e30c018269
Jesse Odenore <info@jesseodenore.com>
2023-04-04 18:35:29 +0100

    Add loading prop for reusability

--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import LoadingPost from "./LoadingPost";
 
 import "./Post.css";
 import PostContainer from "./PostContainer";
-const PostList = ({ posts }) => {
+const PostList = ({ posts, loading }) => {
   return (
     <section className="posts-main">
       <div className="posts-container">
         <div className="posts-grid">
-          {posts.length > 0 ? (
-            posts.map((post) => {
-              return <PostContainer post={post.data} key={post.data.id} />;
-            })
-          ) : (
-            <div>Empty</div>
-          )}
+          {posts.map((post) => {
+            return loading ? (
+              <LoadingPost key={post.data.id} />
+            ) : (
+              <PostContainer post={post.data} key={post.data.id} />
+            );
+          })}
         </div>
       </div>
     </section>
